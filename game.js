87023510a09041f382bc053b6de5bdee7b9d56a4
eduardonk9999 <thiago.novak7@gmail.com>
2023-03-01 13:14:00 -0300

    feat: first script game

---
+++ game.js
@@ -0,0 +1,54 @@
+const container = document.querySelector('.renderGame');
+
+function boardCreat() {
+  for(i = 0; i < 9; i++){
+    const div = document.createElement('div');
+    container.appendChild(div)
+    div.classList.add('cel')
+   
+  }
+}
+
+
+function clickPalyer() {
+  const boxs = document.querySelectorAll('.cel');
+
+  boxs.forEach(box => {
+  
+    box.addEventListener("click", () => {
+      box.innerHTML = 'X'
+      
+      if(event.target.textContent == 'X'){
+        console.log('SOU X') 
+      } else {
+        console.log('SOU O')
+      }
+      
+      // if(box.textContent === 'O') {
+      //   event.target.innerHTML = 'X'
+      // } else {
+      //   event.target.innerHTML = 'O'
+      // }    
+    })
+
+   
+    
+  })
+}
+
+function play() {
+  const victories = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6] 
+  ]
+
+  
+}
+
+
+
+
+boardCreat()
+clickPalyer()
+play()
